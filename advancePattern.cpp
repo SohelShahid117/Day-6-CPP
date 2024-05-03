ec058f81f47750cf6426f00cf9bc07d26f5421b9
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-03 23:43:39 +0600

    4.2 Advanced Pattern Questions in C++  start now

---
+++ advancePattern.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "4.2 Advanced Pattern Questions in C++\n";
+    cout<< "hello world";
+    //https://youtu.be/k9OlCJFy5yo?si=8cqbamivqjfIkr_R ------link
+
+
+return 0;
+}
