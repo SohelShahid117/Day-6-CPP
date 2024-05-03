907c5dbacf50f5c3d7caf96885f039ca2a2ddbbe
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-04 00:33:36 +0600

    4.2 Advanced Pattern 3 min done

--- advancePattern.cpp
+++ advancePattern.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 int main(){
     cout << "4.2 Advanced Pattern Questions in C++\n";
-    cout<< "hello world";
+    //cout<< "hello world";
     //https://youtu.be/k9OlCJFy5yo?si=8cqbamivqjfIkr_R ------link
+
+    cout<<"inverted pattern\n";
+    int n;
+    cout<<"enter a nmbr :";
+    cin>>n;
+    for(int i =n ;i>=1;i--){
+        for(int j =1 ;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
 
 
 return 0;
